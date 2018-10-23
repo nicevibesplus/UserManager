79cacaa3089917594a6d427b7d0fb7bcd1366546
speckij <j_spec05@uni-muenster.de>
2018-10-23 19:47:25 +0200

    * Added support for Content-Type JSON  * Added systemd service File

--- util.go
+++ util.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"errors"
+	"strings"
+	"encoding/json"
 )
 
 func Fail(err error) {
@@ -13,28 +15,48 @@ func Fail(err error) {
 }
 
 func parseUser(r *http.Request) (error, UserCredentials){
-	r.ParseForm()
 
-	username := r.PostForm.Get("username")
-	password := r.PostForm.Get("password")
-	fs := r.PostForm.Get("fs")
-
-	if username == "" || password == "" {
+	if strings.Contains(r.Header.Get("Content-Type"),"application/x-www-form-urlencoded") {
+		r.ParseForm()
+		username := r.PostForm.Get("username")
+		password := r.PostForm.Get("password")
+		fs := r.PostForm.Get("fs")
+		if username == "" || password == "" {
+			return errors.New("Error parsing User to struct"), UserCredentials{}
+		}
+		return nil, UserCredentials{username,password, fs}
+	} else if strings.Contains(r.Header.Get("Content-Type"), "application/json") {
+		var uc UserCredentials
+		decoder := json.NewDecoder(r.Body)
+		decoder.Decode(&uc)
+		if uc.Username == "" || uc.Password == "" {
+			return errors.New("Error parsing User to struct"), UserCredentials{}
+		}
+		return nil, uc
+	} else {
 		return errors.New("Error parsing User to struct"), UserCredentials{}
 	}
-
-	return nil, UserCredentials{username,password, fs}
 }
 
 func parseGroupUser(r *http.Request) (error, GroupUser) {
-	r.ParseForm()
 
-	user := r.PostForm.Get("username")
-	group := r.PostForm.Get("group")
-
-	if user == "" || group == "" {
+	if strings.Contains(r.Header.Get("Content-Type"),"application/x-www-form-urlencoded") {
+		r.ParseForm()
+		user := r.PostForm.Get("username")
+		group := r.PostForm.Get("group")
+		if user == "" || group == "" {
+			return errors.New("Error parsing User to struct"), GroupUser{}
+		}
+		return nil, GroupUser{user,group}
+	} else if strings.Contains(r.Header.Get("Content-Type"), "application/json") {
+		var gc GroupUser
+		decoder := json.NewDecoder(r.Body)
+		decoder.Decode(&gc)
+		if gc.user == "" || gc.group == "" {
+			return errors.New("Error parsing User to struct"), GroupUser{}
+		}
+		return nil, gc
+	} else {
 		return errors.New("Error parsing User to struct"), GroupUser{}
 	}
-
-	return nil, GroupUser{user, group}
 }
